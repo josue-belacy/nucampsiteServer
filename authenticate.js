e26e19d4b9b045d3a039eae49071ae6ecc51474c
Josue Belacy <jbelacy@gmail.com>
2022-04-22 05:54:47 -0400

    Set up the verifyAdmin() middleware

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/users');
+const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
@@ -11,8 +11,8 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-  return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = user => { 
+    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
 };
 
 const opts = {};
@@ -37,4 +37,14 @@ exports.jwtPassport = passport.use(
   )
 );
 
-exports.verifyUser = passport.authenticate('jwt', {session: false})
+exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) {
+    if(req.user.admin) {
+        return next();
+    } else {
+        err = new Error("You are not authorized to perform this operation!");
+        err.status = 403;
+        return next (err);
+    }
+};
