9514e7c7226c342c8904bcfd0545461e942be8a0
Josue Belacy <jbelacy@gmail.com>
2022-04-24 03:33:38 -0400

    CORS

--- routes/users.js
+++ routes/users.js
@@ -1,69 +1,77 @@
-const express = require("express");
-const User = require("../models/user");
-const passport = require("passport");
-const authenticate = require("../authenticate");
+const express = require('express');
+const User = require('../models/user');
+const passport = require('passport');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
 
-const router = express.Router();
+const userRouter = express.Router();
 
 /* GET users listing. */
-router.get("/", authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-  User.find()
-    .then((user) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type", "application/json');
-      res.json(user);
-    })
-    .catch((err) => next(err));
+userRouter.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+    User.find()
+        .then((users) => res.status(200).json(users))
+        .catch((err) => next(err));
 });
 
-router.post("/signup", (req, res) => {
-  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
-    if (err) {
-      res.statusCode = 500;
-      res.setHeader("Content-Type", "application/json");
-      res.json({ err: err });
-    } else {
-      if (req.body.firstname) {
-        user.firstname = req.body.firstname;
-      }
-      if (req.body.lastname) {
-        user.lastname = req.body.lastname;
-      }
-      user.save((err) => {
+userRouter.post('/signup', cors.corsWithOptions, (req, res) => {
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
         if (err) {
-          res.statusCode = 500;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ err: err });
-          return;
+            res.status(500).json({ err: err });
+        } else {
+            if (req.body.firstname) {
+                user.firstname = req.body.firstname;
+            }
+            if (req.body.lastname) {
+                user.lastname = req.body.lastname;
+            }
+            user.save((err) => {
+                if (err) res.status(500).json({ err: err });
+                passport.authenticate('local')(req, res, () => {
+                    res.status(200).json({ success: true, status: 'Registration Successful!' });
+                });
+            });
         }
-        passport.authenticate("local")(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.json({ success: true, status: "Registration Successful!" });
-        });
-      });
+    });
+});
+
+userRouter.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.status(200).json({
+        success: true,
+        token: token,
+        status: 'You are successfully logged in!'
+    });
+});
+
+userRouter.get('/logout', cors.corsWithOptions, (req, res, next) => {
+    if (req.session) {
+        req.session.destroy();
+        res.clearCookie('session-id');
+        res.redirect('/');
+    } else {
+        const err = new Error('You are not logged in!');
+        err.status = 401;
+        return next(err);
     }
-  });
 });
 
-router.post("/login", passport.authenticate("local"), (req, res) => {
-  const token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({ success: true, token: token, status: "You are successfully logged in!" });
+userRouter.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        const token = authenticate.getToken({ _id: req.user._id });
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+    }
 });
 
-router;
-verifyAdmin.get("/logout", (req, res, next) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie("session-id");
-    res.redirect("/");
-  } else {
-    const err = new Error("You are not logged in!");
-    err.status = 403;
-    return next(err);
-  }
-});
+userRouter.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
-module.exports = router;
+userRouter.get(
+    '/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/google/success',
+        failureRedirect: '/google/failure'
+    })
+);
+
+module.exports = userRouter;
